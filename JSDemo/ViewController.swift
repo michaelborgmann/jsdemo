75f7d397283747c7ce9776af59d67b48ec3dfd31
Michael Borgmann <michaelborgmann@mailbox.org>
2017-03-29 15:30:11 +0200

    Invoke JavaScript from iOS app

--- JSDemo/ViewController.swift
+++ JSDemo/ViewController.swift
@@ -7,12 +7,46 @@
 //
 
 import UIKit
+import JavaScriptCore
 
 class ViewController: UIViewController {
+    
+    lazy var context: JSContext? = {
+        let context = JSContext()
+        
+        guard let javaScriptFile = Bundle.main.path(forResource: "jsdemo", ofType: "js") else {
+            print("File not found")
+            return nil
+        }
+        
+        do {
+            // TODO: Contents of URL
+            let script = try String(contentsOfFile: javaScriptFile, encoding: String.Encoding.utf8)
+            _ = context?.evaluateScript(script)
+        } catch (let error) {
+            print("Error processing JavaScript file: \(error)")
+        }
+        
+        return context
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        guard let context = context else {
+            print("Context not found")
+            return
+        }
+        
+        if let hello = context.evaluateScript("helloFromJavaScript") {
+            let value = hello.call(withArguments: nil)
+            print(value!)
+        }
+        
+        if let greet = context.objectForKeyedSubscript("greet") {
+            let value = greet.call(withArguments: ["Michael"]).toString()
+            print(value!)
+        }
     }
 
     override func didReceiveMemoryWarning() {
---
+++ JSDemo/jsdemo.js
@@ -0,0 +1,9 @@
+function helloFromJavaScript() {
+    return "Hello from JavaScript";
+}
+
+function greet(name) {
+    return "Hello " + name;
+}
+
+greet("world!");
